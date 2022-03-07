35264fc338050d73519c84a72458caf1522419ab
Jun <jun.w4ng@gmail.com>
2022-03-07 17:14:51 -0300

    Testing heroku

---
+++ get-todos.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const { Client } = require("pg");
+const db = async () => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  await client.connect();
+  const res = await client.query("SELECT * from todos");
+  await client.end();
+  return res.rows;
+};
+
+module.exports = db;
---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const handlebars = require("express-handlebars");
+const getTodos = require("./get-todos");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use("/static", express.static("public"));
+
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
+
+app.get("/", async (req, res) => {
+  const todos = await getTodos();
+  res.render("index", { todos });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
